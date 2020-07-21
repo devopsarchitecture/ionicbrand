641bc62793379f30118be6d8772767849800a41f
devopsarchitecture <devopsarchitecture@gmail.com>
2020-07-21 17:12:02 +0530

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -5,12 +5,15 @@ module.exports = function (config) {
   config.set({
     basePath: '',
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    browsers: ['ChromeHeadless'],
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
+      require('karma-phantomjs-launcher'),
+      require('karma-coverage'),
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
@@ -26,6 +29,17 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['Chrome'],
+    customLaunchers: {
+       ChromeHeadless: {
+            base: 'Chrome',
+            flags: [
+            '--headless',
+            '--disable-web-security',
+            '--disable-setuid-sandbox',
+            '--no-sandbox',
+            '--remote-debugging-port=9222',]
+      }
+},
     singleRun: false
   });
 };
